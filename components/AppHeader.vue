93dc699571336c3150e05ecec4133364dd93a436
Kirill Morgunov <kirillmorgunov@mail.ru>
2025-03-29 16:46:31 +0400

    first commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <div class="logo">Qtim</div>
+    <nav class="navigation">
+        <div class="navigation__item">Works</div>
+        <div class="navigation__item">About</div>
+        <button>Let’s work</button>
+    </nav>
+  </header>
+</template>
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+</script>
+<template>
+    <h1>Index page</h1>
+</template>
