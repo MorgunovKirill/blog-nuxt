0b835e56e41e0a8d121498a9bd8ae3030b0804cc
Kirill Morgunov <kirillmorgunov@mail.ru>
2025-03-31 11:08:58 +0400

    upd deploy file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/blog-nuxt/'
-  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui'],
